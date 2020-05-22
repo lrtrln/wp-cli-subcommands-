122df8b1620effc0613d46ee9f389e6ba7fe865a
LRTRLN <lt@lrtrln.fr>
2020-05-22 18:40:03 +0200

    first commit

---
+++ complete.sh
@@ -0,0 +1,42 @@
+_wp_complete() {
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	local command=${COMP_WORDS[1]}
+	local subcommand=${COMP_WORDS[2]}
+
+	COMPREPLY=""
+
+	if [[ "$command" = "plugin" ]]
+	then
+		case "$subcommand" in
+		activate|deactivate|update|delete|get|is-active|path|status|uninstall|verify-checksums)
+			COMPREPLY=( $(cd $(wp plugin path); compgen -d -- "${cur}" ) )
+			;;
+		*)
+		esac
+	elif [[ "$command" = "theme" ]]
+	then
+		case "$subcommand" in
+		activate|delete|disable|enable|is-active|is-installed|path|status|update)
+			COMPREPLY=( $(cd $(wp theme path); compgen -d -- "${cur}" ) )
+			;;
+		*)
+		esac
+	fi
+	if [[ "$COMPREPLY" = "" ]]
+	then
+		local opts="$(wp cli completions --line="$COMP_LINE" --point="$COMP_POINT")"
+
+		if [[ "$opts" =~ \<file\>\s* ]]
+		then
+			COMPREPLY=( $(compgen -f -- $cur) )
+		elif [[ $opts = "" ]]
+		then
+			COMPREPLY=( $(compgen -f -- $cur) )
+		else
+			COMPREPLY=( ${opts[*]} )
+		fi
+	fi
+
+	return 0
+}
+complete -o nospace -F _wp_complete wp
